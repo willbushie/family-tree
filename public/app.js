f300a69621b1407725a2bb3e00fb93681db53975
William Bushie <willbushie@gmail.com>
2025-06-26 18:01:49 -0500

    create and style containers to hold person info

--- public/app.js
+++ public/app.js
@@ -1,12 +1,33 @@
 fetch('/api/people')
-  .then(response => response.json())
-  .then(data => {
-    const container = document.getElementById('people-container');
-    data.forEach(person => {
-      const div = document.createElement('div');
-      div.textContent = `${person.fname}
-            ${person.lname}`;
-      container.appendChild(div);
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('people-container');
+
+        data.forEach(person => {
+            // build the container for each person in the databse
+            const card = document.createElement('div');
+            card.className = 'person-card';
+            
+            const avatar = document.createElement('img');
+            avatar.src = '/default-avatar.jpg';
+            card.appendChild(avatar);
+
+            const name = document.createElement('h3');
+            name.textContent = `${person.fname} ${person.lname}`;
+            card.appendChild(name);
+
+            const dob = document.createElement('p');
+            const personDOB = new Date(person.date_of_birth);
+            dob.textContent = `${personDOB.toDateString()}`;
+            card.appendChild(dob);
+
+            const age = document.createElement('p');
+            const personAge = Math.round((new Date() - personDOB)/31556952000);
+            age.textContent = `(age ${personAge})`;
+            card.appendChild(age);
+
+
+            container.appendChild(card);
+        });
     });
-  });
 
